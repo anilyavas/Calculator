60e5eecc21a125c558656129d7765ef065873ef3
Anıl <anil@Anl-MacBook-Air.local>
2023-10-09 15:31:23 +0300

    finished app

--- App.js
+++ App.js
@@ -1,20 +1,111 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { useState } from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
 export default function App() {
+  const [input, setInput] = useState();
+  const [result, setResult] = useState(0);
+  const onButtonPress = (value) => {
+    if (value === '=') {
+      try {
+        setResult(eval(input));
+      } catch (error) {
+        setResult('error');
+      }
+    } else if (value === 'C') {
+      setInput('');
+      setResult('');
+    } else {
+      setInput(input + value);
+    }
+  };
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
       <StatusBar style="auto" />
-    </View>
+      <View style={styles.resultContainer}>
+        <Text style={styles.resultText}>{result}</Text>
+      </View>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.inputText}
+          value={input}
+          onChangeText={setInput}
+          keyboardType="numeric"
+        ></TextInput>
+      </View>
+      <View style={styles.buttonContainer}>
+        {[
+          '7',
+          '8',
+          '9',
+          '/',
+          '4',
+          '5',
+          '6',
+          '*',
+          '1',
+          '2',
+          '3',
+          '-',
+          '0',
+          'C',
+          '=',
+          '+',
+        ].map((item, index) => (
+          <TouchableOpacity
+            key={index}
+            style={styles.button}
+            onPress={() => onButtonPress(item)}
+          >
+            <Text style={styles.buttonText}>{item}</Text>
+          </TouchableOpacity>
+        ))}
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+  },
+  resultContainer: {
+    flex: 2,
     justifyContent: 'center',
+    alignItems: 'flex-end',
+  },
+  resultText: {
+    fontSize: 40,
+  },
+  inputContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'flex-end',
+  },
+  inputText: {
+    fontSize: 30,
+  },
+  buttonContainer: {
+    flex: 7,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+  },
+  button: {
+    fontSize: 24,
+    width: '25%',
+    height: '20%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderWidth: 1,
+    borderColor: '#ccc',
+  },
+  buttonText: {
+    fontSize: 24,
   },
 });
